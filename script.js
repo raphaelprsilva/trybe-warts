bff29cf7e67ffff1251e3b0005f5e8e4cf2ec017
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-11-04 20:15:21 -0300

    Add unfinished refactored code

--- script.js
+++ script.js
@@ -1,140 +1,68 @@
-const formLogin = document.querySelector('.trybewarts-login');
-const conteudo = document.querySelector('.conteudo-enviar');
-const formAluno = document.querySelector('#evaluation-form');
-const inputEmail = document.querySelector('#login');
-const inputSenha = document.querySelector('#senha');
-const termos = document.querySelector('#agreement');
-const botaoEnvia = document.querySelector('#submit-btn');
-const textArea = document.querySelector('#textarea');
-const tamanhoTextoAtual = document.querySelector('#counter');
+const trybewartsForm = document.querySelector('[data-js="trybewarts-form"]');
+const evaluationForm = document.querySelector('[data-js="evaluation-form"]');
+const agreementCheck = document.querySelector('[data-js="agreement-input"]');
+const submitButton = document.querySelector('[data-js="submit-btn"]');
+const commentsTextArea = document.querySelector('[data-js="comments-textarea"]');
+const charactersCount = document.querySelector('[data-js="counter"]');
+const radioFamilyItems = document.querySelectorAll('[data-js="radio-family"]');
+const contentToLearn = document.querySelectorAll('[data-js="learn-input"]');
 
-// console.log(conteudo);
-// console.log(formAluno.elements[7].checked);
-// console.log(formAluno.elements);
+const defaultEmail = 'tryber@teste.com';
+const defaultPassword = '123456';
+const maxCommentsCharactersAllowed = '500';
 
-function verificaLogin(event) {
+trybewartsForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  const email = inputEmail.value;
-  const senha = inputSenha.value;
-  const loginPadrao = 'tryber@teste.com';
-  const senhaPadrao = '123456';
+  const inputedEmail = event.target.email.value;
+  const inputedPassword = event.target.senha.value;
+  const isAValidEmail = defaultEmail === inputedEmail;
+  const isAvalidPassword = defaultPassword === inputedPassword;
+  const isAValidLogin = isAValidEmail && isAvalidPassword;
 
-  if (email !== loginPadrao || senha !== senhaPadrao) {
-    alert('Login ou senha inválidos.');
-  } else {
+  if (isAValidLogin) {
     alert('Olá, Tryber!');
-  }
-}
-
-function verificaTermos() {
-  if (termos.checked) {
-    botaoEnvia.disabled = false;
-  } else { botaoEnvia.disabled = true; }
-}
-
-verificaTermos();
-
-function contaCaracteres() {
-  let atual = tamanhoTextoAtual.innerHTML;
-  atual = 500;
-  tamanhoTextoAtual.innerHTML = atual - textArea.value.length;
-}
-
-contaCaracteres();
-
-function pegaInfoNomeEmailCasa() {
-  const nomeCompleto = document.createElement('p');
-  const email = document.createElement('p');
-  const casa = document.createElement('p');
-
-  const nomeSobrenomeForm = `Nome: ${formAluno.elements[0].value} ${formAluno.elements[1].value}`;
-  nomeCompleto.innerHTML = nomeSobrenomeForm;
-  conteudo.appendChild(nomeCompleto);
-  conteudo.insertBefore(nomeCompleto, conteudo.childNodes[0]);
-
-  const emailForm = `Email: ${formAluno.elements[2].value}`;
-  email.innerHTML = emailForm;
-  conteudo.appendChild(email);
-  conteudo.insertBefore(email, conteudo.childNodes[1]);
-
-  const casaForm = `Casa:  ${formAluno.elements[3].value}`;
-  casa.innerHTML = casaForm;
-  conteudo.appendChild(casa);
-  conteudo.insertBefore(casa, conteudo.childNodes[2]);
-
-  return (nomeSobrenomeForm, emailForm, casaForm);
-}
-
-function pegaFamilia() {
-  const familia = document.createElement('p');
-
-  for (let index = 4; index < 7; index += 1) {
-    if (formAluno.elements[index].checked) {
-      const familiaForm = `Família:  ${formAluno.elements[index].value}`;
-      familia.innerHTML = familiaForm;
-      conteudo.appendChild(familia);
-      conteudo.insertBefore(familia, conteudo.childNodes[3]);
-    }
-  }
-}
-
-function pegaMaterias() {
-  const materias = document.createElement('p');
-  const materiasForm = [];
-
-  for (let index = 7; index < 13; index += 1) {
-    if (formAluno.elements[index].checked) {
-      materiasForm.push(formAluno.elements[index].value);
-    }
-  }
-
-  const materiasSelecionadas = `Matérias: ${materiasForm.join(', ')}`;
-
-  materias.innerHTML = materiasSelecionadas;
-  conteudo.appendChild(materias);
-  conteudo.insertBefore(materias, conteudo.childNodes[4]);
-
-  return materiasSelecionadas;
-}
-
-function pegaAvaliacao() {
-  const avaliacao = document.createElement('p');
-
-  for (let index = 13; index < 23; index += 1) {
-    if (formAluno.elements[index].checked) {
-      const avaliacaoForm = `Avaliação: ${formAluno.elements[index].value}`;
-      avaliacao.innerHTML = avaliacaoForm;
-      conteudo.appendChild(avaliacao);
-      conteudo.insertBefore(avaliacao, conteudo.childNodes[5]);
-    }
-  }
-}
-
-function pegaComentario() {
-  const comentario = document.createElement('p');
-
-  const comentarioForm = `Observações: ${formAluno.elements[23].value}`;
-  comentario.innerHTML = comentarioForm;
-  conteudo.appendChild(comentario);
-  conteudo.insertBefore(comentario, conteudo.childNodes[6]);
-
-  return comentarioForm;
-}
-
-formAluno.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  pegaInfoNomeEmailCasa();
-  pegaFamilia();
-  pegaMaterias();
-  pegaAvaliacao();
-  pegaComentario();
-
-  for (let index = 1; index < formAluno.children.length; index += 1) {
-    formAluno.children[index].innerHTML = '';
+  } else {
+    alert('Login ou senha inválidos.');
   }
 });
 
-textArea.addEventListener('keyup', contaCaracteres);
-termos.addEventListener('click', verificaTermos);
-formLogin.addEventListener('submit', verificaLogin);
+agreementCheck.addEventListener('click', (event) => {
+  const targetElement = event.target.checked;
+  if (targetElement) {
+    submitButton.removeAttribute('disabled');
+    submitButton.classList.remove('disabled');
+  } else {
+    submitButton.setAttribute('disabled', '');
+    submitButton.classList.add('disabled');
+  }
+});
+
+commentsTextArea.addEventListener('input', (event) => {
+  const commentsLength = event.target.value.length;
+  charactersCount.textContent = maxCommentsCharactersAllowed - commentsLength;
+});
+
+submitButton.addEventListener('click', () => {
+  const userFirstName = evaluationForm.inputName.value;
+  const userLastName = evaluationForm.inputLastname.value;
+  const userEmail = evaluationForm.email.value;
+
+  const userHouseOptions = evaluationForm.house.options;
+  const userHouseSelectedIndex = userHouseOptions.selectedIndex;
+  const selectedHouse = userHouseOptions[userHouseSelectedIndex].value;
+
+  let familyOption = '';
+
+  for (let index = 0; index < radioFamilyItems.length; index += 1) {
+    const isCurrentItemChecked = radioFamilyItems[index].checked;
+    if (isCurrentItemChecked) {
+      familyOption = radioFamilyItems[index].value;
+    }
+  }
+
+  console.log(familyOption);
+
+  console.log([...contentToLearn].filter((element) => element.checked));
+});
+
+console.log(contentToLearn);
